8a5a29c2426025453eefabd2ebad7b851c3c52f6
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-20 22:20:53 +0500

    Conditional Statements

---
+++ script.js
@@ -0,0 +1,13 @@
+//  Question 1
+
+document.write( "<h1>" + "Question 1" + "<br>" + "<br>" + "</h1>");
+var city = "Karachi";
+var userInput = prompt("Enter your city");
+
+ if(city === "Karachi"){
+    document.write("Welcome to city of lights"+ "<br>" + "<br>" );
+ }
+
+//   Question 2
+
+ document.write( "<h1>" + "Question 2" + "<br>" + "<br>" + "</h1>");
