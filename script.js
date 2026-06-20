e5835a060761c623711db86c9268eeebc0103223
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-20 23:02:32 +0500

    Conditional Statements

--- script.js
+++ script.js
@@ -1,13 +1,131 @@
-//  Question 1
+// //  Question 1
 
-document.write( "<h1>" + "Question 1" + "<br>" + "<br>" + "</h1>");
-var city = "Karachi";
-var userInput = prompt("Enter your city");
+// document.write( "<h1>" + "Question 1" + "<br>" + "<br>" + "</h1>");
+// var city = "Karachi";
+// var userInput = prompt("Enter your city");
 
- if(city === "Karachi"){
-    document.write("Welcome to city of lights"+ "<br>" + "<br>" );
- }
+//  if(city === "Karachi"){
+//     document.write("Welcome to city of lights"+ "<br>" + "<br>" );
+//  }
 
-//   Question 2
+// //   Question 2
 
- document.write( "<h1>" + "Question 2" + "<br>" + "<br>" + "</h1>");
+//  document.write( "<h1>" + "Question 2" + "<br>" + "<br>" + "</h1>");
+ 
+//  var gender = prompt("What is your gender?");
+
+//  if(gender === "male"){
+//     document.write("Good Morning Sir");
+//  }
+//  if(gender === "female"){
+//     document.write("Good morning Ma'am");
+//  }
+
+// //  Question 3
+
+//  document.write( "<h1>" + "Question 3" + "<br>" + "<br>" + "</h1>");
+
+//   var trafficLight = prompt("Enter traffic signal color");
+
+//   if(trafficLight === "Red"){
+//     document.write("Must Stop");
+//   }
+//  if(trafficLight === "Yellow"){
+//     document.write("Ready to move");
+//  }
+//  if(trafficLight === "Green"){
+//     document.write("Move now");
+//  }
+ 
+// //  Question 4 
+
+//  document.write( "<h1>" + "Question 4" + "<br>" + "<br>" + "</h1>");
+
+//  var reaminingFuel = prompt("Enter remaining fuel in liters");
+
+//  if(reaminingFuel < 0.25){
+//     document.write("Please refill  the fuel your car");
+//  }
+
+//  Question 5
+
+document.write( "<h1>" + "Question 5" + "<br>" + "<br>" + "</h1>");
+
+//  Q5 (a)
+
+
+  var a = 4;
+  if (++a === 5){
+     alert("given condition for variable a is true"); 
+    } 
+
+    // Output
+    //  Messege displayed
+
+
+//  Q5 (b)
+
+  var b = 82;
+  if (b++ === 83)
+    {
+     alert("given condition for variable b is true");
+  } 
+
+    // Output
+    // Message not displyed 
+ 
+ 
+//   Q5 (c)
+
+  var c = 12;
+   if (c++ === 13)
+    { alert("condition 1 is true"); 
+
+    } 
+    if (c === 13){ 
+        alert("condition 2 is true");
+     } 
+     if (++c < 14){ 
+        alert("condition 3 is true");
+     } 
+     if(c === 14){
+         alert("condition 4 is true"); 
+  } 
+
+        //    Output
+      //  Condition 4 is true 
+
+// Q5 (d)
+
+ var materialCost = 20000;
+  var laborCost = 2000; 
+  var totalCost = materialCost + laborCost;
+   if (totalCost === laborCost + materialCost){
+
+     alert("The cost equals");
+
+     } 
+
+    //  Output
+    // Material equals 
+
+//  Q5 (e)
+  
+  if (true){ 
+    alert("True"); 
+} if (false){ 
+    alert("False");
+ } 
+
+    //   Output
+    // True 
+
+// Q5(f)
+
+ if("car" < "cat"){
+     alert("car is smaller than cat");
+     } 
+
+    //  Output 
+    // Message displayed
+ 
